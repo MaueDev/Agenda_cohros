b0c7e3be98383e9ed5da2a7e090aac421f430feb
MaueDev <mauricio.tecnologia@troupebrasil.com.br>
2024-02-06 18:38:02 -0300

    feat: criando ambiente frontend com vue.js e implementando ele na diretorio frontend/

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <router-view class="main_content"></router-view>
+</template>
+
+<script>
+import Router from './router';
+
+export default {
+  name: 'App',
+  router: Router
+};
+</script>
+
+<style>
+:root{
+  ---beckgroud: #f8f9fa;
+  ---light-sky-blue: #e6f7ff;
+}
+.main_content{
+  background-color: var(---beckgroud);
+}
+</style>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/home/HelloWorld.vue';
+
+const routes = [{
+
+    path: '/',
+    name: 'home',
+    component:Home
+},
+{
+
+    path: '/teste',
+    name: 'hometeste',
+    component:Home
+}
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
